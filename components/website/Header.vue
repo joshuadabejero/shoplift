ce1680206abe64e3ba5d3e282d62a2b6e42df6c0
joshuadabejero <joshuadabejero@gmail.com>
2025-05-26 00:59:13 +0800

    feat: add navigation bar for desktop view

---
+++ components/website/Header.vue
@@ -0,0 +1,67 @@
+<script setup></script>
+
+<template>
+  <v-app-bar class="navigation-bar" color="black">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon
+        class="icon__container"
+        icon="mdi-cart-outline"
+        color="black"
+        :ripple="false"
+      ></v-app-bar-nav-icon>
+      <v-app-bar-title class="navigation__title">ShopLift</v-app-bar-title>
+    </template>
+
+    <v-btn :ripple="false">Home</v-btn>
+    <v-btn :ripple="false">Products</v-btn>
+    <v-btn :ripple="false">Categories</v-btn>
+
+    <template v-slot:append>
+      <v-btn to="/" icon="mdi-magnify" :ripple="false"></v-btn>
+      <v-btn icon="mdi-cart-outline" :ripple="false"></v-btn>
+      <v-btn icon="mdi-account-outline" :ripple="false"></v-btn>
+    </template>
+  </v-app-bar>
+</template>
+
+<style scoped>
+/* Base Styling */
+
+/* Custom Styles */
+.icon__container {
+  border-radius: 6px;
+  background-color: #eab308;
+  margin-right: 8px;
+  padding: 8px;
+}
+
+.navigation__title {
+  color: #eab308;
+  font-weight: 700;
+  line-height: 28px;
+}
+
+/* Component Overrides */
+.navigation-bar > ::v-deep(.v-toolbar__content) {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 0 16px;
+  height: 72px !important;
+}
+
+.navigation-bar ::v-deep(.v-btn) {
+  text-transform: none !important;
+}
+
+.navigation-bar ::v-deep(.v-btn:hover) {
+  color: #eab308;
+}
+
+.navigation-bar ::v-deep(.v-btn__overlay),
+.navigation-bar ::v-deep(.v-btn__underlay) {
+  display: none !important;
+  pointer-events: none !important;
+}
+
+/* Mobile Responsiveness */
+</style>
